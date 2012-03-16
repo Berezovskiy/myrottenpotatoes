6343834be3233b03ac5c61f61455010c00f10a1f
Oleksandr Berezovskyy <berezovskiy@gmail.com>
2012-03-16 01:34:43 -0300

    fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
 
   def index
     col = params[:orderby]
-    sel_ratings = params[:ratings].values if params[:ratings]
+    sel_ratings = params[:ratings].keys if params[:ratings]
     @movies = nil
     if col
       @movies = (@movies ||= Movie).order(col)
