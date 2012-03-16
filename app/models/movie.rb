109d6d579f18c52b7213bf46996d83714a80360f
Oleksandr Berezovskyy <berezovskiy@gmail.com>
2012-03-15 23:34:58 -0300

    fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   def self.get_all_ratings
-    self.find :all, :select => "rating", :group => "rating"
+    self.find(:all, :select => "rating", :group => "rating").map(&:rating)
   end
 end
